3d924382ec48bc16f154dccd0b6ce942a45fe2b3
arshiyaniveus <arshiya.u@niveussolutions.com>
2023-08-23 19:44:25 +0530

    html file chnaged to text files

--- AsyncAwait/index.js
+++ AsyncAwait/index.js
@@ -13,17 +13,17 @@ fsFile.opendirSync(folderName);
 
 (function testFunction () {
     fsFile.readdir(folderName, async (err, files) => {
-        createNewFile('index1.html') // execute
+        createNewFile('index1.txt') // execute
         setTimeout(() => {
             console.log("index 2 file creation without using async function--->")
-            createNewFile('index2.html')
+            createNewFile('index2.txt')
         }, 6000)
         await new Promise((resolve) => setTimeout(() => {
             console.log("index 2 file creation using async function --->");
-            resolve(createNewFile('index2.html'))
+            resolve(createNewFile('index2.txt'))
         }, 5000)); // 1-second delay
 
-        createNewFile('index3.html')
+        createNewFile('index3.txt')
     })
 }())
 
@@ -31,8 +31,8 @@ fsFile.opendirSync(folderName);
 
 function createNewFile (file1) {
 
-    const html = `<h1>Hello world</h1>`
-    fsFile.appendFile(folderName + '/' + file1, html, (err) => {
+    const textfile = `<h1>Hello world</h1>`
+    fsFile.appendFile(folderName + '/' + file1, textfile, (err) => {
         if (err) throw err;
     });
     console.log(` file saved ${file1}`);
