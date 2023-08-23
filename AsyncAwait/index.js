72e7cc6701129d9af3461530c7eb779b7a378d27
arshiyaniveus <arshiya.u@niveussolutions.com>
2023-08-23 20:17:08 +0530

    html file chnaged to text files

--- AsyncAwait/index.js
+++ AsyncAwait/index.js
@@ -1,40 +1,39 @@
-const { rejects } = require('assert');
-const fsFile = require('fs');
-const { resolve } = require('path');
+const fs = require('fs');
 const folderName = 'public';
 
+// Checking whether folder exists
+console.log(fs.existsSync(folderName));
 
-//checking whether folderExist
-console.log(fsFile.existsSync(folderName))
+fs.opendirSync(folderName);
 
+(async function asyncFunction () {
+    fs.readdir(folderName, async (err, files) => {
+        try {
+            createNewFile('index1.txt'); // execute
 
-fsFile.opendirSync(folderName);
+            setTimeout(() => {
+                console.log("index 2 file creation without using async function--->");
+                createNewFile('index2.txt');
+            }, 6000);
 
+            await new Promise((resolve) => setTimeout(() => {
+                console.log("index 2 file creation using async function --->");
+                resolve(createNewFile('index2.txt'));
+            }, 5000)); // 5-second delay
 
-(function testFunction () {
-    fsFile.readdir(folderName, async (err, files) => {
-        createNewFile('index1.txt') // execute
-        setTimeout(() => {
-            console.log("index 2 file creation without using async function--->")
-            createNewFile('index2.txt')
-        }, 6000)
-        await new Promise((resolve) => setTimeout(() => {
-            console.log("index 2 file creation using async function --->");
-            resolve(createNewFile('index2.txt'))
-        }, 5000)); // 1-second delay
-
-        createNewFile('index3.txt')
-    })
-}())
-
-
-
-function createNewFile (file1) {
-
-    const textfile = `<h1>Hello world</h1>`
-    fsFile.appendFile(folderName + '/' + file1, textfile, (err) => {
-        if (err) throw err;
+            createNewFile('index3.txt');
+        } catch (error) {
+            console.error('Error:', error);
+        }
     });
-    console.log(` file saved ${file1}`);
+})();
 
+function createNewFile (file) {
+    try {
+        const textFile = `<h1>Hello world</h1>`;
+        fs.appendFileSync(folderName + '/' + file, textFile);
+        console.log(`File saved ${file}`);
+    } catch (error) {
+        console.error('Error:', error);
+    }
 }
--- Promises.js
+++ Promises.js
@@ -1,21 +1,19 @@
+try {
+    const promise1 = new Promise((resolve, reject) => {
+        if (true) {
+            resolve('promise message!!!')
+        } else {
+            reject('not Promised!!!');
+        }
+    });
 
-const promise1 = new Promise((resolve, reject) => {
-    if (true) {
-        resolve('promise message!!!')
+    promise1.then(val => {
+        console.log(val);
+    }).catch(err => {
+        console.log(err);
+    });
 
-    } else {
-        reject('not Promised!!!');
-    }
-})
-
-promise1.then(val => {
-    console.log(val)
+    console.log(promise1);
+} catch (error) {
+    console.error('Error:', error);
 }
-).catch(err => {
-    console.log(err)
-})
-console.log(promise1);
-
-
-
-
